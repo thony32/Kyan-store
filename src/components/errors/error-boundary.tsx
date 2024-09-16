30f3704d7aaf0c220d2fd03bc06339d2b9b805cb
Anthony <mahefasoanyrianaanthony09@gmail.com>
2024-09-16 20:41:49 +0300

    Project clean

--- src/components/errors/error-boundary.tsx
+++ src/components/errors/error-boundary.tsx
@@ -2,7 +2,7 @@ import { Button } from '../ui/button'
 
 const ErrorFallback = () => {
     return (
-        <div className="flex flex-col items-center justify-center w-screen h-screen text-red-500" role="alert">
+        <div className="flex flex-col items-center justify-center w-screen h-screen text-red-500">
             <h2 className="text-lg font-semibold">Oops, something went wrong :( </h2>
             <Button className="mt-4" onClick={() => window.location.assign(window.location.origin)}>
                 Refresh
