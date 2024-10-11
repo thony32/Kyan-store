2b33ebfa6c7a9af3e1c7f034ffaea3445e07c8bc
Notoavina3X <notoavinarazafilalao@gmail.com>
2024-10-11 11:58:56 +0300

    clean up

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,6 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
     readonly VITE_API_URL: string
-    readonly VITE_SUPABASE_URL: string
-    readonly VITE_SUPABASE_KEY: string
-    readonly VITE_BOT_API
 }
 
 interface ImportMeta {
